458c0c28981386c37b1ed5d2c4d124d293747519
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-01 17:51:28 -0600

    data sources added

--- public_html/drawMap.js
+++ public_html/drawMap.js
@@ -1,49 +1,72 @@
-document.write("begin script <br>");
-var w = 1000;
-var h = 600;
-var projection = d3.geo.equirectangular()
-	.center([-106.02, 34.17])
-	.translate([w / 2, h / 2])
-	.scale([4200]);
-document.write("scale = " + projection.scale() + "<br>");
-document.write("center = " + projection.center() + "<br>");
-var NMPath = d3.geo.path().projection(projection); // use d3.geoPath()?
+/**
+ * Load USGS Hydrologic data set data (www.nhd.usgs.org) (converted to json).
+ * Draw a state map outline (www.census.gov/go/www/cob/cbf_state.html), and the rivers inside the state boundary.
+ * GDB file converted to shapefile by gdal
+ * Shaefiles converted to GeoJSON using gdal ogr2ogr
+ */
 
-var riverPath = d3.geo.path().projection(projection);
 
 // global variables to hold json geographic data
 var nmBoundaryJSON;
 var riversJSON;
 
 
-//document.onload = function(){
+function drawMap() {
+	document.write("begin script <br>");
+	var w = 1000;
+	var h = 600;
+	var projection = d3.geo.equirectangular()
+		.center([-106.02, 34.17])
+		.translate([w / 2, h / 2])
+		.scale([4200]);
+	document.write("scale = " + projection.scale() + "<br>");
+	document.write("center = " + projection.center() + "<br>");
+	var NMPath = d3.geo.path().projection(projection); // use d3.geoPath()?
+
+	var riverPath = d3.geo.path().projection(projection);
+
+
+
+
+
 // Draw NM State Boundary
-d3.json("nm-boundary.json", function(json) {
-	// path is a property of the svg
-	// essentially creates enough paths for the data (?)
-	svg.selectAll("path")
-		.data(json.features)
-		.enter()
-		.append("path")
-		.attr("d", NMPath)
-		.attr("stroke", "black")
-		.attr("stroke-width", "3")
-		.attr("fill", "none");
+	d3.json("data/nm-boundary.json", function(json) {
+		// path is a property of the svg
+		// essentially creates enough paths for the data (?)
+		svg.selectAll("path")
+			.data(json.features)
+			.enter()
+			.append("path")
+			.attr("d", NMPath)
+			.attr("stroke", "black")
+			.attr("stroke-width", "3")
+			.attr("fill", "none");
 
-	nmBoundaryJSON = json;
-});
+		nmBoundaryJSON = json;
+	});
 
-// Draw Rivers - or something
-d3.json("flowline.json", function(jsonData) {
+// Draw Rivers
+	d3.json("data/NHDFlowline.json", function(jsonData) {
 
-	riversJSON = jsonData;
+		riversJSON = jsonData;
 
-	//filterRiversOnFlow();
-	//filterRiversOnInState();
+		// remove points from the paths if they are not
+		// TODO within the state boundary,
+		// and if they are not
+		// sufficiently large enough flow.
+		//filterRiversOnFlow();
+		//filterRiversOnInState();
 
-	drawRiversSVG();
+		drawRiversSVG();
 
-});
+	});
+
+	var svg = d3.select("body")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+}
 
 function drawRiversSVG() {
 
@@ -62,9 +85,5 @@ function filterRiversOnFlow() {
 
 }
 
-var svg = d3.select("body")
-	.append("svg")
-	.attr("width", w)
-	.attr("height", h);
 
-//}
+
